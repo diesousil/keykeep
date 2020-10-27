b5268d21f0e681a51b292bbdd58dfc34ab56b960
Diego Souza Silva <diesousil@gmail.com>
2020-10-27 11:15:17 -0300

    Customized template colors and changed menu elements disposition. Created top menu bar on layout directory, this file has been applied on master layout file, so we can use it as user options access and titles area.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,47 +9,87 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?php echo URL::to('/'); ?>/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?php echo URL::to('/'); ?>/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  <!-- customization -->
-  <link rel="stylesheet" href="/css/customizations.css">
 
   <!-- jsGrid -->
-  <link rel="stylesheet" href="../../public/plugins/jsgrid/jsgrid.min.css">
-  <link rel="stylesheet" href="../../public/plugins/jsgrid/jsgrid-theme.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../../public/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?php echo URL::to('/'); ?>/plugins/jsgrid/jsgrid.min.css">
+  <link rel="stylesheet" href="<?php echo URL::to('/'); ?>/plugins/jsgrid/jsgrid-theme.min.css">
+
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  <!-- customization -->
+  <link rel="stylesheet" href="<?php echo URL::to('/'); ?>/css/customizations.css?v=<?php echo rand(0,10000000);?>">
+
 </head>
 <body class="hold-transition sidebar-mini">
 <!-- Site wrapper -->
 <div class="wrapper">
-  
+  @section('topbar')
+    @include("layouts.topbar")
+  @show
   @section('sidebar')
-	@include("layouts.sidebar")
+	  @include("layouts.sidebar")
   @show
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          
+          <div class="col-sm-6">
+            <h1>{{$title}}</h1>
+          </div>
+          
+          <!--
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Advanced Form</li>
+            </ol>
+          </div>
+          -->
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
     <!-- Main content -->
     <section class="content">
 
-      <!-- Default box -->
-      <div class="card">
+    <div class="container-fluid">
+      <div class="card card-default">
+        
+        <!-- /.card-header -->
         <div class="card-header">
-          <h3 class="card-title">{{$title}}</h3>
+          <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+              <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+            </div>
         </div>
+        
+        <!-- /.card-body -->
         <div class="card-body">
           @yield('content')
         </div>
-        <!-- /.card-body -->
+
+        <div class="card-footer">
+            
+        </div>
+
+        
+      </div>
+    </div>
+
+      <!-- Default box -->
+      <div class="card">
+        
       </div>
       <!-- /.card -->
 
@@ -66,11 +106,11 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="/plugins/jquery/jquery.min.js"></script>
+<script src="<?php echo URL::to('/'); ?>/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo URL::to('/'); ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="/js/adminlte.min.js"></script>
+<script src="<?php echo URL::to('/'); ?>/js/adminlte.min.js"></script>
 
 </body>
 </html>
---
+++ resources/views/layouts/topbar.blade.php
@@ -0,0 +1,40 @@
+<nav class="main-header navbar navbar-expand navbar-black navbar-dark">
+
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+        </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        
+        <!-- User Dropdown Menu -->
+        <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#">
+                <i class="fa fa-arrow-down"></i>
+                
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <span class="dropdown-item dropdown-header">Diego Silva</span>
+                
+                <div class="dropdown-divider"></div>
+                
+                <a href="#" class="dropdown-item">
+                    <i class="fa fa-key mr-2"></i> Change Password
+                </a>
+
+                <a href="#" class="dropdown-item">
+                    <i class="fas fa-users mr-2"></i> 8 friend requests
+                </a>
+
+                <a href="#" class="dropdown-item">
+                    <i class="fas fa-file mr-2"></i> 3 new reports
+                </a>
+                
+            </div>
+        </li>
+        
+    </ul>
+</nav>
